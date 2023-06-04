53b005b7455d812e8d6f803ca128a1263b9fd857
reiner850593913 <lk850593913@gmail.com>
2023-06-04 13:26:59 +0800

    feat: providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,54 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // {
+    //   provide: AppService,
+    //   useClass: AppService,
+    // },
+    {
+      provide: 'person',
+      useValue: {
+        name: 'aaa',
+        age: 20,
+      },
+    },
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'bbb',
+          desc: 'ccc',
+        };
+      },
+    },
+    {
+      provide: 'person3',
+      useFactory(person: { name: string }, appService: AppService) {
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['person', AppService],
+    },
+    {
+      provide: 'person4',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+        });
+        return {
+          name: 'bbb',
+          desc: 'ccc',
+        };
+      },
+    },
+    {
+      provide: 'person5',
+      useExisting: 'person2',
+    },
+  ],
 })
 export class AppModule {}
